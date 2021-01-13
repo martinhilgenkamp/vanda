ea69d2488ffaa80c7a3870b2a75ae1409122c9f4
Gerben Gaastra <ggaastra@bloemert.com>
2021-01-13 14:46:26 +0100

    Cleaned up publish script

--- publish/publish.ps1
+++ publish/publish.ps1
@@ -4,10 +4,9 @@ $path = "../"
 $DateTime = (Get-Date -Format "yyyyMMddHHmmss")
 $destination = "../artifacts\archive-$DateTime.zip"
 # exclusion rules. Can use wild cards (*)
-$exclude = @(".vscode","publish","artifacts",".gitignore")
+$exclude = @(".vscode","publish","artifacts",".gitignore",".git")
 # get files to compress using exclusion filer
 $files = Get-ChildItem -Path $path -Exclude $exclude
-echo $files
 New-Item ../artifacts -ItemType Directory -ea 0
 # compress
 Compress-Archive -Path $files -DestinationPath $destination -CompressionLevel Fastest
