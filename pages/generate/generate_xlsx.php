b69606620ff46bf5cabe23978c93e404a02c8bba
Martin Hilgenkamp <mhilgenkamp@pruim.nl>
2023-09-04 11:40:34 +0200

    added phpspreadsheet class

--- pages/generate/generate_xlsx.php
+++ pages/generate/generate_xlsx.php
@@ -131,18 +131,20 @@ $objPHPExcel->setActiveSheetIndex(0);
 
 // Redirect output to a client’s web browser (Excel2007)
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="F00830'.date('ymd').'.xlsx"');
+header('Content-Disposition: attachment;filename="F00830'.date('ymd').'.xls"');
 header('Cache-Control: max-age=0');
 // If you're serving to IE 9, then the following may be needed
-header('Cache-Control: max-age=1');
+// header('Cache-Control: max-age=1');
 
 // If you're serving to IE over SSL, then the following may be needed
-header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
-header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-header ('Pragma: public'); // HTTP/1.0
+//header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+//header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
+//header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+//header ('Pragma: public'); // HTTP/1.0
+$ob_get_status = ob_get_status();
+if($ob_get_status['name']!='zlib output compression')ob_clean();
 
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
 $objWriter->save('php://output');
 exit;
 
