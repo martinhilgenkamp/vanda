0548f07ae935c51551d0e18ceb6589e91a6924f7
Martin Hilgenkamp <mhilgenkamp@pruim.nl>
2024-05-06 10:47:38 +0200

    Fix CSV bug

--- pages/csv.php
+++ pages/csv.php
@@ -4,8 +4,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Noodzakelijke dingen bij elkaar rapen.
-require '../../vendor/autoload.php';
-require_once('../../inc/class/class.db.php');
+require '../vendor/autoload.php';
+require_once('../inc/class/class.db.php');
 date_default_timezone_set("Europe/Amsterdam");
 
 
