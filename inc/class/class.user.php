9826885eab29c5a099934e356439502c7c3e263b
martinhilgenkmap <martin@mdsystems.nl>
2024-11-01 13:41:10 +0100

    Fixed broken usermanagement, need to fix workorder user registration

--- inc/class/class.user.php
+++ inc/class/class.user.php
@@ -4,30 +4,8 @@ require_once("class.db.php");
 class UserManager {
 	private $db;
 
-	public $username;
-	public $name;
-	public $email;
-	public $id;
-	public $active;
-	public $level;
-
 	function __construct($username = null) {
 		$this->db = new DB();
-		if($username){
-			$qry = "SELECT * FROM vanda_user WHERE username = '".$username."' LIMIT 1";
-			if ($result = $this->db->link->query($qry)) {
-				if ($result->num_rows > 0) {
-					while ($row = $result->fetch_assoc()) {
-						$this->id = $row['id'];
-						$this->name = $row['username'];
-						$this->email = $row['email'];
-						$this->level = $row['level'];
-						$this->active = $row['active'];
-						$this->username = $row['username'];
-					}
-				}
-			}
-		}
 	}
 
 	function checkLogin($data){ 
@@ -37,10 +15,10 @@ class UserManager {
 			$password = $data['password'];
 			
 			//3.1.2 Checking the values are existing in the database or not
-			$this->username = $this->getUserByName($username);
+			$user = $this->getUserByName($username);
 
 			//3.1.2 If the posted values are equal to the database values, then session will be created for the user.
-			if ($this->name && $this->checkCredentials($this->name, $password)){
+			if ($user && $this->checkCredentials($user, $password)){
 				$_SESSION['username'] = $username;
 				setcookie('username',$_SESSION['username'],time()+30*24*60*60);
 			}else{
@@ -51,8 +29,8 @@ class UserManager {
 		
 		//Check if cookie is there.		
 		if (isset($_COOKIE['username'])){
-			$this->name = $this->getUserByName($_COOKIE['username']);
-			if ($this->name){
+			$user = $this->getUserByName($_COOKIE['username']);
+			if ($user){
 				$_SESSION['username'] = $_COOKIE['username'];				
 				setcookie('username',$_COOKIE['username'],time()+30*24*60*60);
 				return true;
@@ -74,11 +52,12 @@ class UserManager {
 
 	function updateHash($password, $userId) {
 		$hash = password_hash($password, PASSWORD_DEFAULT);
-		
+
 		/* Update the password hash on the database. */
 		$data = ["password" => $hash];
 		$where = 'id = '.$userId;
 		$this->db->updateQuery('vanda_user', $data, $where);
+
 	}
 
 	function checkCredentials($user, $password) {
@@ -110,7 +89,7 @@ class UserManager {
 	}
 
 	function getUserByName($username) {
-		$qry = "SELECT * FROM vanda_user WHERE username = '".$username."'";
+		$qry = "SELECT id, username, email, level, active, password FROM vanda_user WHERE username = '".$username."'";
 		$res = $this->db->selectQuery($qry);
 		return count($res) > 0 ? $res[0] : null;
 	}	
@@ -131,8 +110,7 @@ class UserManager {
 
     function listUsers() {
         $qry = "SELECT * FROM `vanda_user` WHERE active = 1";
-        
-        return c($qry);
+        return $this->db->selectQuery($qry);
     }
 
     function getUserById($id) {
